88340167f3d41533a1fa8089b89c593edaff54f4
m.artur <m.artur@abilities.space>
2021-12-26 16:58:52 +0700

    Remove sync methods and try catch

--- DbrainApi/DbrainApi.cs
+++ DbrainApi/DbrainApi.cs
@@ -24,135 +24,65 @@ namespace DbrainApi
         }
 
         /// <inheritdoc/>
-        public string Classify(byte[] passData, string fileExtension = ".jpg")
-        {
-            try
-            {
-                var url = $"{_baseUrl}/classify?token={_token}&return_crops=false";
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), url))
-                {
-                    request.Headers.TryAddWithoutValidation("accept", "application/json");
-                    var multipartContent = new MultipartFormDataContent();
-                    var file1 = new ByteArrayContent(passData);
-                    file1.Headers.Add("Content-Type", "image/jpeg");
-                    var fileName = Guid.NewGuid().ToString().Replace("-", "") + fileExtension;
-                    multipartContent.Add(file1, "image", fileName);
-                    request.Content = multipartContent;
-
-                    var response = _httpClient.SendAsync(request).GetAwaiter().GetResult();
-                    return response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                }
-            }
-            catch (Exception ex)
-            {
-                return "{\"Message\":" + $"{ex.Message} {ex.InnerException?.Message}" + "}";
-            }
-        }
-
-        /// <inheritdoc/>
         public async Task<string> ClassifyAsync(byte[] passData, string fileExtension = ".jpg")
         {
-            try
+            var url = $"{_baseUrl}/classify?token={_token}&return_crops=false";
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"), url))
             {
-                var url = $"{_baseUrl}/classify?token={_token}&return_crops=false";
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), url))
-                {
-                    request.Headers.TryAddWithoutValidation("accept", "application/json");
+                request.Headers.TryAddWithoutValidation("accept", "application/json");
 
-                    var multipartContent = new MultipartFormDataContent();
-                    var file1 = new ByteArrayContent(passData);
-                    file1.Headers.Add("Content-Type", "image/jpeg");
-                    var fileName = Guid.NewGuid().ToString().Replace("-", "") + fileExtension;
-                    multipartContent.Add(file1, "image", fileName);
-                    request.Content = multipartContent;
+                var multipartContent = new MultipartFormDataContent();
+                var file1 = new ByteArrayContent(passData);
+                file1.Headers.Add("Content-Type", "image/jpeg");
+                var fileName = Guid.NewGuid().ToString().Replace("-", "") + fileExtension;
+                multipartContent.Add(file1, "image", fileName);
+                request.Content = multipartContent;
 
-                    var response = await _httpClient.SendAsync(request);
-                    return await response.Content.ReadAsStringAsync();
-                }
-            }
-            catch (Exception ex)
-            {
-                return "{\"Message\":\"" + $"{ex.Message} {ex.InnerException?.Message}" + "\"}";
-            }
-        }
-
-        /// <inheritdoc/>
-        public string Recognize(byte[] passData, bool async = false, bool hands = false, bool handsAsync = false, string fileExtension = ".jpg")
-        {
-            try
-            {
-                var url = $"{_baseUrl}/recognize?token={_token}&return_crops=false" +
-                            $"&async={async.ToString().ToLower()}" +
-                            $"&with_hitl={hands.ToString().ToLower()}" +
-                            $"&hitl_async={handsAsync.ToString().ToLower()}";
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), url))
-                {
-                    request.Headers.TryAddWithoutValidation("accept", "application/json");
-
-                    var multipartContent = new MultipartFormDataContent();
-                    var file1 = new ByteArrayContent(passData);
-                    file1.Headers.Add("Content-Type", "image/jpeg");
-                    var fileName = Guid.NewGuid().ToString().Replace("-", "") + fileExtension;
-                    multipartContent.Add(file1, "image", fileName);
-                    request.Content = multipartContent;
-
-                    var response = _httpClient.SendAsync(request).GetAwaiter().GetResult();
-                    return response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                }
-            }
-            catch (Exception ex)
-            {
-                return "{\"Message\":\"" + $"{ex.Message} {ex.InnerException?.Message}" + "\"}";
+                var response = await _httpClient.SendAsync(request).ConfigureAwait(false);
+                return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             }
         }
 
         /// <inheritdoc/>
         public async Task<string> RecognizeAsync(byte[] passData, bool async = false, bool hands = false, bool handsAsync = false, string fileExtension = ".jpg")
         {
-            try
+            var url = $"{_baseUrl}/recognize?token={_token}&return_crops=false" +
+                "&doc_type=passport_main" +
+                "&doc_type=passport_main_handwritten" +
+                "&hitl_required_fields=date_of_birth&hitl_required_fields=date_of_issue" +
+                "&hitl_required_fields=first_name&hitl_required_fields=issuing_authority" +
+                "&hitl_required_fields=other_names&hitl_required_fields=place_of_birth" +
+                "&hitl_required_fields=sex&hitl_required_fields=subdivision_code" +
+                "&hitl_required_fields=surname&hitl_required_fields=series_and_number" +
+                $"&async={async.ToString().ToLower()}" +
+                $"&with_hitl={hands.ToString().ToLower()}" +
+                $"&hitl_async={handsAsync.ToString().ToLower()}";
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"), url))
             {
-                var url = $"{_baseUrl}/recognize?token={_token}&return_crops=false" +
-                            $"&async={async.ToString().ToLower()}" +
-                            $"&with_hitl={hands.ToString().ToLower()}" +
-                            $"&hitl_async={handsAsync.ToString().ToLower()}";
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), url))
-                {
-                    request.Headers.TryAddWithoutValidation("accept", "application/json");
+                request.Headers.TryAddWithoutValidation("accept", "application/json");
 
-                    var multipartContent = new MultipartFormDataContent();
-                    var file1 = new ByteArrayContent(passData);
-                    file1.Headers.Add("Content-Type", "image/jpeg");
-                    var fileName = Guid.NewGuid().ToString().Replace("-", "") + fileExtension;
-                    multipartContent.Add(file1, "image", fileName);
-                    request.Content = multipartContent;
+                var multipartContent = new MultipartFormDataContent();
+                var file1 = new ByteArrayContent(passData);
+                file1.Headers.Add("Content-Type", "image/jpeg");
+                var fileName = Guid.NewGuid().ToString().Replace("-", "") + fileExtension;
+                multipartContent.Add(file1, "image", fileName);
+                request.Content = multipartContent;
 
-                    var response = await _httpClient.SendAsync(request);
-                    return await response.Content.ReadAsStringAsync();
-                }
-            }
-            catch (Exception ex)
-            {
-                return "{\"Message\":\"" + $"{ex.Message} {ex.InnerException?.Message}" + "\"}";
+                var response = await _httpClient.SendAsync(request).ConfigureAwait(false);
+                return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             }
         }
 
         /// <inheritdoc/>
         public async Task<string> ResultAsync(string taskId)
         {
-            try
+            var url = $"{_baseUrl}/result/{taskId}?return_crops=false&token={_token}";
+            using (var request = new HttpRequestMessage(new HttpMethod("GET"), url))
             {
-                var url = $"{_baseUrl}/result/{taskId}?return_crops=false&token={_token}";                
-                using (var request = new HttpRequestMessage(new HttpMethod("GET"), url))
-                {
-                    request.Headers.TryAddWithoutValidation("accept", "application/json");
+                request.Headers.TryAddWithoutValidation("accept", "application/json");
 
-                    var response = await _httpClient.SendAsync(request);
-                    return await response.Content.ReadAsStringAsync();
-                }                
-            }
-            catch (Exception ex)
-            {
-                return "{\"Message\":\"" + $"{ex.Message} {ex.InnerException?.Message}" + "\"}";
+                var response = await _httpClient.SendAsync(request).ConfigureAwait(false);
+                return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             }
         }
     }
--- DbrainApi/IDbrainApi.cs
+++ DbrainApi/IDbrainApi.cs
@@ -12,24 +12,7 @@ namespace DbrainApi
         /// </summary>
         /// <param name="passData">Контент файла.</param>
         /// <param name="fileExtension">Расширение файла.</param>
-        string Classify(byte[] passData, string fileExtension = ".jpg");
-
-        /// <summary>
-        /// Распознавание типа документа.
-        /// </summary>
-        /// <param name="passData">Контент файла.</param>
-        /// <param name="fileExtension">Расширение файла.</param>
         Task<string> ClassifyAsync(byte[] passData, string fileExtension = ".jpg");
-
-        /// <summary>
-        /// Распознавание полей из документа.
-        /// </summary>
-        /// <param name="passData">Контент файла.</param>
-        /// <param name="async">Асинхронный автоматическое распознаванеи.</param>
-        /// <param name="hands">С ручным распознаванием.</param>
-        /// <param name="handsAsync">Асинхронное ручное распознавание.</param>
-        /// <param name="fileExtension">Расширение файла.</param>
-        string Recognize(byte[] passData, bool async = false, bool hands = false, bool handsAsync = false, string fileExtension = ".jpg");
 
         /// <summary>
         /// Распознавание полей из документа.
